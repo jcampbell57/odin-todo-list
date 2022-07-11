bcbb72f9a4b20e0433605372c123daf08df2527c
jcampbell <jcampbell57@gmail.com>
2022-07-10 17:10:42 -0700

    includes barebones elements and styling

--- src/core.js
+++ src/core.js
@@ -1,14 +1,40 @@
+const body = document.querySelector('body');
 
-const _displayTitle = () => {
-    const body = document.querySelector('body');
-    let title = document.createElement('h1');
+const _createHeader = () => {
+    const header = document.createElement('header')
+    
+    // display title
+    const title = document.createElement('h1');
     title.textContent = 'Things To Do';
-    body.appendChild(title);
+    header.appendChild(title);
+    
+    body.appendChild(header);
+}
+
+const _createFooter = () => {
+    const footer = document.createElement('footer')
+
+    const copyright = document.createElement('p');
+    copyright.textContent = `Copyright © ${new Date().getFullYear()} jcampbell57`;
+  
+    const githubLink = document.createElement('a');
+    githubLink.href = 'https://github.com/jcampbell57';
+    githubLink.target = '_blank';
+  
+    const githubIcon = document.createElement('img');
+    githubIcon.src = '../src/assets/GitHub-light-32px.png';
+    githubIcon.setAttribute('class', 'github');
+
+    githubLink.appendChild(githubIcon);
+    footer.appendChild(copyright);
+    footer.appendChild(githubLink);
+    body.appendChild(footer);
 }
 
 
 const initialize = () => {
-    _displayTitle();    
+    _createHeader();
+    _createFooter();
 };
 
 export default initialize;
